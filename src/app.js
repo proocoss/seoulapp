a31c3dc7e6af75729f62b1ded217d675695c5271
proocoss <proocoss@gmail.com>
2017-10-06 02:16:30 +0900

    update seoulapp code - ympage 부분 적용

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-import React, { Component } from "react";
+// npm modules
+import React, {Component} from "react";
+import {createStore, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
+import {Router, Route, IndexRoute, browserHistory} from "react-router";
+import ReduxThunk from 'redux-thunk';
 
 class App extends Component {
 
