e0b8e83f8a0273d04aef9a046b927be1ca3e9ecb
proocoss <proocoss@gmail.com>
2017-10-12 18:07:50 +0900

    update webpack config - dev, prod css modules local name 패턴 변경

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -168,7 +168,7 @@ module.exports = {
                   importLoaders: 1,
                   // [jh.lee] css module setting
                   modules: true,
-                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                  localIdentName: '[local]--[hash:base64:5]',
                 },
               },
               {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -180,7 +180,7 @@ module.exports = {
                         sourceMap: shouldUseSourceMap,
                         // [jh.lee] css module setting
                         modules: true,
-                        localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                        localIdentName: '[local]--[hash:base64:5]',
                       },
                     },
                     {
