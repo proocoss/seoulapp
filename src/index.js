788197ec51535da4fef8b6fa1d6fc73047e16c0d
proocoss <proocoss@gmail.com>
2017-10-18 18:20:36 +0900

    update index.js for firebase - firebase 임시 주석

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import registerServiceWorker from "registerServiceWorker";
 
 // firebase module for login
-import firebase from "fire";
+//import firebase from "fire";
 
 // user modules
 import App from "app";
@@ -14,26 +14,26 @@ import App from "app";
 // style
 import "../node_modules/reset-css/reset.css";
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    //let token = result.credential.accessToken;
-    console.log("success");
-    // ...
-  }
-  // The signed-in user info.
-  //let user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  //let errorCode = error.code;
-  //let errorMessage = error.message;
-  // The email of the user's account used.
-  //let email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  //let credential = error.credential;
-  // ...
-  console.log("fail");
-});
+// firebase.auth().getRedirectResult().then(function(result) {
+//   if (result.credential) {
+//     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+//     //let token = result.credential.accessToken;
+//     console.log("success");
+//     // ...
+//   }
+//   // The signed-in user info.
+//   //let user = result.user;
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   //let errorCode = error.code;
+//   //let errorMessage = error.message;
+//   // The email of the user's account used.
+//   //let email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   //let credential = error.credential;
+//   // ...
+//   console.log("fail");
+// });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
