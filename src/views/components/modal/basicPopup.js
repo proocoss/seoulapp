fa97767b30367bb00f45322c0ed9ee46f6b8f181
proocoss <proocoss@gmail.com>
2017-10-24 16:49:55 +0900

    add basic popup - 기본 팝업 파일 추가

---
+++ src/views/components/modal/basicPopup.js
@@ -0,0 +1,34 @@
+// npm modules
+import React, {Component} from "react";
+import classNames from 'classnames/bind';
+
+// style
+import moreType1 from "./moreType1.css";
+
+// image
+import btnMoreIcon1 from "assets/images/btn-more-icon-1.png";
+
+const st = classNames.bind(moreType1);
+
+class MoreType1 extends Component {
+    /**
+     * Life cycle func
+    */
+    constructor(props) {
+        console.log("constructor MoreType1");
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div 
+                className={ st(this.props.type) }
+                onClick={this.props.requestList}>
+                {this.props.value} <img className={ st("more-icon") } src={btnMoreIcon1} alt="more-icon" />
+            </div>
+        );
+    }
+}
+
+export default MoreType1;
