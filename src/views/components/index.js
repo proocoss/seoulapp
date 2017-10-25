c3c40e61d6cb57e305a3ee6d1121f5dce0964ace
proocoss <proocoss@gmail.com>
2017-10-25 23:55:33 +0900

    add information popup - 프로그램 소개 팝업 추가

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -3,4 +3,5 @@ export {default as CommonHeader} from "./header/commonHeader";
 export {default as MoreType1} from "./button/moreType1";
 export {default as LoadingType1} from "./loading/loadingType1";
 export {default as BasicPopup} from "./modal/basicPopup";
+export {default as InformationPopup} from "./modal/informationPopup";
 export {default as Empty} from "./dummy/empty";
